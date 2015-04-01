faa21998a49b71445e6a6f9e1e08bcdc5cef5f37
Denis Pauk <pauk.denis@gmail.com>
2015-04-01 15:12:56 +0300

    cfy-2153: save manager context

--- manager_blueprint/scripts/configure.py
+++ manager_blueprint/scripts/configure.py
@@ -4,11 +4,16 @@ import json
 import fabric
 
 import vcloud_plugin_common
+from cloudify import ctx
+from network_plugin import get_network_name
 
+
+PROVIDER_CONTEXT_RUNTIME_PROPERTY = 'provider_context'
 
 def configure(vcloud_config):
     _copy_vsphere_configuration_to_manager(vcloud_config)
     _update_vm()
+    _save_context()
 
 
 def _copy_vsphere_configuration_to_manager(vcloud_config):
@@ -35,3 +40,30 @@ def _update_vm():
         # * zram-config for minimize out-of-memory cases with zswap
         # * other packages for create deployments from source
         fabric.api.run("sudo apt-get install zram-config gcc python-dev libxml2-dev libxslt-dev -q -y 2>&1")
+
+
+def _save_context():
+
+    resources = dict()
+
+    node_instances = ctx._endpoint.storage.get_node_instances()
+    nodes_by_id = \
+        {node.id: node for node in ctx._endpoint.storage.get_nodes()}
+
+    for node_instance in node_instances:
+        run_props = node_instance.runtime_properties
+        props = nodes_by_id[node_instance.node_id].properties
+
+        if "management_network" == node_instance.node_id:
+            resources['int_network'] = {
+                "name": props.get('resource_id'),
+                "use_external_resource": props.get('use_external_resource')
+            }
+        if "manager_floating_ip" == node_instance.node_id:
+            resources['floating_ip'] = {
+                "ip": run_props.get('public_ip')
+            }
+
+    return {
+        'resources': resources
+    }
