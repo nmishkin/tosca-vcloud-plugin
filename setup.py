cd1ba1a6410db5987051a92af498f8b1c108d4cf
Nat Mishkin <nmishkin@vmware.com>
2015-05-07 22:52:36 -0400

    Bump pyvcloud version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     description='Cloudify plugin for vmWare vCloud infrastructure.',
     install_requires=[
         'cloudify-plugins-common==3.2a6',
-        'pyvcloud.mishkin>=12.1',
+        'pyvcloud.mishkin>=12.2',
         'requests==2.4.3',
         'IPy==0.81'
     ]
