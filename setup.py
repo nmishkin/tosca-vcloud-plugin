f663dd47a9ff463dcdc369e6959a9f27967ecb85
Nat Mishkin <nmishkin@vmware.com>
2015-05-20 10:48:51 -0400

    Update version info for pyvcloud dependency

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ setup(
     description='Cloudify plugin for vmWare vCloud infrastructure.',
     install_requires=[
         'cloudify-plugins-common==3.2a6',
-#        'pyvcloud>=13rc1',
-        'pyvcloud.mishkin>=12.5',
+        'pyvcloud>=13rc2',
         'requests==2.4.3',
         'IPy==0.81'
     ]
